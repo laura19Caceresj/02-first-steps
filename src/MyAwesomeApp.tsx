5f1ce439b821c45ec1ffeed2337c9e83a1efa7f2
laura19Caceresj <lauracaceresj19@gmail.com>
2026-06-11 18:41:10 -0500

    feature/element event
    Feat: Add handleClick for increment button

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,45 +1,49 @@
 import type { CSSProperties } from "react";
 
- const firstName = 'Eduar';
-    const lastName = 'Herrera';
+const firtName = "Laura";
+const lastName = "Caceres";
 
-    const favoriteGames = ["Final Fantasy", "Metal Gear", "Resident Evil"];
-    const isActive = false;
-
-
-
-    const address = {
-        zipCode: 'ABC-123',
-        country: 'Colombia',
-    };
+const favoriteGames = ["Elden Ring", "Smash", "Metal Gear"];
+const isActive = true;
 
 
-//componente con funcion de flecha
+const address = {
+    zipCode: "ABC-123",
+    country: "Colombia",
+};
 
-const   myStyles :CSSProperties= {
-        backgroundColor: 'red',
-            borderRadius: 10,
-                padding: 10,
-                marginTop:30,
-                };
+
+
+
 
 
 export const MyAwesomeApp = () => {
 
-return (
-    <>
-        <h1>{firstName} </h1>
-        <h3>{lastName}</h3>
-        <p>{favoriteGames.join(",")}</p>
-        <p>{2 + 2}</p>
-//ternario java script
-        <h1>{isActive ? "Active" : "Inactive"}</h1>
+    const myStyles: CSSProperties = {
 
-        <p style={myStyles}>{JSON.stringify(address)}</p>
+        backgroundColor: 'green',
+        borderRadius: 20,
+        padding: 10,
+        marginTop: 50,
 
 
-    </>
-)
+
+    }
+
+    return (
+        <>
+            <h1>{firtName}</h1>
+            <h3>{lastName}</h3>
+
+            <p>{favoriteGames.join(",")}</p>
+            <p>{2 + 2}</p>
+
+
+            <h1>{isActive ? "Activo" : "No activo"}</h1>
+
+            <p style={myStyles}>{JSON.stringify(address)} </p>
+        </>
+    )
 }
 
 
@@ -49,22 +53,12 @@ return (
 
 
 
+//export function MyAwesomeApp(){
+//  return(
 
-
-
-//componente tarea componente normal
-//export function MyAwesomeApp() {
-
-
-
-
-//  return (
 //    <>
-//      <h1>Fernado</h1>
-//    <h3>Herrera</h3>
-
+//  <h1>Laura</h1>
+//<h3>Caceres</h3>
 //</>
-
-// )
-
+//)
 //}
--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,40 +1,47 @@
-
-
 interface Props {
   name: string;
   quantity?: number;
 }
 
-
-
-
 export const ItemCounter = ({ name, quantity }: Props) => {
+
+  const handleCLick = () => {
+    console.log(`Click en ${name}`)
+  }
+
+
+
+
+
+
   return (
-    <section style={{
-      display: 'flex',
-      alignItems: 'center',
-      gap: 10,
-      marginTop: 10
-    }}
+    <section
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: 10,
+        marginTop: 10,
+      }}
     >
-      <span style={{
-        width: 90,
-      }}>
+      <span
+        style={{
+          width: 150,
+        }}
+      >
         {name}
       </span>
-      <button>+1</button>
+      <button onClick={handleCLick}>+1</button>
       <span>{quantity}</span>
       <button>-1</button>
     </section>
+
   );
 
 
-};
 
+}
 
-
-
-
-
-
-
+//activa boton con solo pasar el mouse por encima
+// onMouseEnter={() => {
+//console.log(`Mouse enter ${name}`);
+//</section>}}
