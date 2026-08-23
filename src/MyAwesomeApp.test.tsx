cf47e21c885d380f1e2c580eb88e503e9cc98107
laura19Caceresj <lauracaceresj19@gmail.com>
2026-08-22 20:56:55 -0500

    feature/Search-for-elements
    Search for elements in the rendered component
    describe
    expect
    screen

--- src/MyAwesomeApp.test.tsx
+++ src/MyAwesomeApp.test.tsx
@@ -1,16 +1,38 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { MyAwesomeApp } from "./MyAwesomeApp";
 import { render, screen } from '@testing-library/react';
 
+
 describe('MyAwesomeApp', () => {
     test('should render firstName and lastName', () => {
+        const { container } = render(<MyAwesomeApp />);
 
+        // screen.debug();
 
+        // console.log(container.innerHTML);
+
+        const h1 = container.querySelector('h1');
+        const h3 = container.querySelector('h3');
+
+        expect(h1?.innerHTML).toContain('Laura');
+        expect(h3?.innerHTML).toContain('Caceres');
+
+    });
+
+    test('should render firstName and lastName - screen', () => {
         render(<MyAwesomeApp />);
 
         screen.debug();
 
         // console.log(container.innerHTML);
+
+        // const h1 = screen.getByRole('heading', {
+        // level: 1,
+        // });
+        // console.log(h1.innerHTML);
+
+        const h1 = screen.getByTestId('first-name-title');
+        expect(h1.innerHTML).toContain('Laura');
 
     });
 })
