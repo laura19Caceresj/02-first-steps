97cb2088e86210f2a35015ec0e6e7a4b14c81e24
laura19Caceresj <lauracaceresj19@gmail.com>
2026-09-15 23:36:52 -0500

    feature/Espect-specific-arguments
    - Add snapshot test for FirstStepsApp
    - Verify correct number of ItemCounter renders (3)
    - Verify ItemCounter is called with correct props for each item
    - Mock ItemCounter component to isolate FirstStepsApp logic
    - Add afterEach hook to clear mocks between tests"

--- src/FirstStepsApp.test.tsx
+++ src/FirstStepsApp.test.tsx
@@ -1,16 +1,41 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, test, vi } from "vitest";
+import { afterEach, describe, expect, test, vi } from "vitest";
 import { FirstStepsApp } from "./FirstStepsApp";
-import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+
+const mockItemCounter = vi.fn((_props: unknown) => {
+    return (
+        <div data-testid="ItemCounter" />
+    )
+
+});
 
 vi.mock('./shopping-cart/ItemCounter', () => ({
-    ItemCounter: () => <div data-testid="ItemCounter" />,
+    ItemCounter: (props: unknown) => mockItemCounter(props),
 }));
 
 
 
+
+
+
+//vi.mock('./shopping-cart/ItemCounter', () => ({
+// ItemCounter: (props: unknown) => (
+//     <div
+//         data-testid="ItemCounter"
+//         name={props.name}
+//         quantity={props.quantity}
+//     />
+//   ),
+// }));
+
+
+
 describe('FirstStepsApp', () => {
+
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
 
     test('should match snapshot', () => {
         const { container } = render(<FirstStepsApp />);
@@ -25,13 +50,30 @@ describe('FirstStepsApp', () => {
         expect(ItemCounters.length).toBe(3);
 
 
-        // screen.debug();
+    });
+
+    test('should render ItemCounter with correct props', () => {
+
+        render(<FirstStepsApp />)
 
 
+        expect(mockItemCounter).toHaveBeenCalledTimes(3);
+        expect(mockItemCounter).toHaveBeenCalledWith(
+            { name: "Nintendo Switch  2", quantity: 1 }
+        );
+
+        expect(mockItemCounter).toHaveBeenCalledWith(
+            { name: "Pro Controller", quantity: 2 }
+        );
+
+        expect(mockItemCounter).toHaveBeenCalledWith(
+            { name: "Super Smash", quantity: 5 }
+        );
 
 
 
 
     })
+
 
 });
